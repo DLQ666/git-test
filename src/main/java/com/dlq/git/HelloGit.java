dd5fee5565d072b5f7c7225b12cef6441e495b13
D·Teach <1114626450@qq.com>
2021-01-21 10:18:31 +0800

    创建项目

---
+++ src/main/java/com/dlq/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.dlq.git;
+
+/**
+ *@program: git-project
+ *@description:
+ *@author: Hasee
+ *@create: 2021-01-21 10:15
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
