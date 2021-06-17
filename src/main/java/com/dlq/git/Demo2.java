4db537ab87fbbf8beacb6a7c436fc6f877e40a26
DLQ <1114626450@qq.com>
2021-06-17 15:53:40 +0800

    桌面库提交者上传代码

---
+++ src/main/java/com/dlq/git/Demo2.java
@@ -0,0 +1,13 @@
+package com.dlq.git;
+
+/**
+ *@program: git-test
+ *@description:
+ *@author: Hasee
+ *@create: 2021-06-17 15:53
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("桌面库提交者上传代码");
+    }
+}
